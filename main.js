9931014e92572e4d6a6e5909b7a3c06aa8b80d02
Nate-Kootstra <144453231+Nate-Kootstra@users.noreply.github.com>
2023-09-21 09:14:53 -0400

    Fixed Blacklight Bug.
    Moving it would make toggling it to flashlight require double click.

---
+++ main.js
@@ -0,0 +1,24 @@
+
+function display(event) {
+    let X = event.clientX;
+    let Y = event.clientY;
+
+    if(!document.getElementById('flashlight').classList.contains('flashlight') && !document.getElementById('flashlight').classList.contains('reverse_flashlight')) {
+        document.getElementById('flashlight').classList.add('flashlight');
+    }
+
+    document.getElementById('flashlight').style.left=X -50 + "px";
+    document.getElementById('flashlight').style.top=Y -50 + "px";
+}
+
+function toggle_flashlight(){
+    if(document.getElementById('flashlight').classList.contains('flashlight')) {
+        document.getElementById('flashlight').classList.add('reverse_flashlight');
+        document.getElementById('flashlight').classList.remove('flashlight');
+    }
+    else {
+        document.getElementById('flashlight').classList.remove('reverse_flashlight');
+        document.getElementById('flashlight').classList.add('flashlight');
+        
+    }
+}
