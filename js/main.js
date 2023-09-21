f3238569c0fe7f433c844eeb004466a2419d5033
Nate-Kootstra <144453231+Nate-Kootstra@users.noreply.github.com>
2023-09-21 08:17:34 -0400

    Uploaded Yesterday's Build
    All of this was programmed last class, but a power reset right before the period ended meant I couldn't upload it. 
    
    Mostly just added a cool invert effect that follows your cursor.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-document.write("Template Loaded.")
+
+function display(event) {
+    let X = event.clientX;
+    let Y = event.clientY;
+
+    
+    document.getElementById('invert').style.left=X -50 + "px";
+    document.getElementById('invert').style.top=Y -50 + "px";
+}
+
+while(true){
+    display()
+}
