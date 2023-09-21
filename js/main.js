a2d3f479c46155ac1d4db204f9f44e5ef107ded7
Nate-Kootstra <144453231+Nate-Kootstra@users.noreply.github.com>
2023-09-21 09:10:48 -0400

    Added Flashlight/Blacklight.
    ARG? Maybe. Not yet anyways.
    
    Cool way to hide text.
    
    Maybe I'll use it at some point.

--- js/main.js
+++ js/main.js
@@ -3,11 +3,22 @@ function display(event) {
     let X = event.clientX;
     let Y = event.clientY;
 
-    
-    document.getElementById('invert').style.left=X -50 + "px";
-    document.getElementById('invert').style.top=Y -50 + "px";
+    if(!document.getElementById('flashlight').classList.contains('flashlight')) {
+        document.getElementById('flashlight').classList.add('flashlight');
+    }
+
+    document.getElementById('flashlight').style.left=X -50 + "px";
+    document.getElementById('flashlight').style.top=Y -50 + "px";
 }
 
-while(true){
-    display()
+function toggle_flashlight(){
+    if(document.getElementById('flashlight').classList.contains('flashlight')) {
+        document.getElementById('flashlight').classList.add('reverse_flashlight');
+        document.getElementById('flashlight').classList.remove('flashlight');
+    }
+    else {
+        document.getElementById('flashlight').classList.remove('reverse_flashlight');
+        document.getElementById('flashlight').classList.add('flashlight');
+        
+    }
 }
